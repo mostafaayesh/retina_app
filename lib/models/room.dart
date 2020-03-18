79344b5b24bcd74822cdb3f34be8fa23f9cfde33
jzpnhlim <50381643+jzpnhlim@users.noreply.github.com>
2020-03-18 13:53:08 -0400

    room modal rev 1

--- lib/models/room.dart
+++ lib/models/room.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:retina/routing/json_nominatim.dart';
 
 class Room {
   String name = "placeholder";
@@ -6,6 +7,7 @@ class Room {
   double y;
   double latitude;
   double longitude;
+  String type = "general";
   String info = "No information available";
 
   Room({
@@ -14,8 +16,32 @@ class Room {
     this.y,
     this.latitude,
     this.longitude,
+    this.type,
     this.info,
   });
+
+  Map<String, dynamic> toMap(){
+    var map = <String, dynamic>{
+      'name':name,
+      'x':x,
+      'y':y,
+      'latitude':latitude,
+      'longitude':longitude,
+      'type':type,
+      'info':info,
+    };
+    return map;
+  }
+
+  Room.frommap(Map<String, dynamic> map){
+    name = map['id'];
+    x = map['x'];
+    y = map['y'];
+    latitude = map['latitude'];
+    longitude = map['longitude'];
+    type = map['type'];
+    info = map['info'];
+  }
 }
 
 class RoomModel extends ChangeNotifier {
