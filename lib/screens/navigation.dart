b3dbfb90d35906bec563001ac145e2b037d581c7
SteveLoveWork <54871601+SteveLoveWork@users.noreply.github.com>
2020-03-19 17:06:30 -0400

    Revert "Revert "Added Text to Speech""
    This reverts commit c3a546a0e569106f4ca57ed3d0f0a953fcfe9db1.

--- lib/screens/navigation.dart
+++ lib/screens/navigation.dart
@@ -5,20 +5,53 @@ import 'package:retina/models/room.dart';
 import 'package:retina/utils/attitude.dart';
 import 'package:retina/widgets/card_tile.dart';
 import 'package:retina/widgets/instruction.dart';
+import 'package:flutter_tts/flutter_tts.dart';
+
+import 'package:logging/logging.dart';
 
 class NavigationScreen extends StatefulWidget {
   @override
   _NavigationScreenState createState() => _NavigationScreenState();
 }
 
+// Check if the user is within 3.0 meters of the anchor and determine whether it should inform the user of the next instruction
+int getUpdate(double xPos, double yPos, double xAnchor, double yAnchor, int instNum){
+  if( getDistance(xPos, xAnchor, yPos, yAnchor) < 3.0 )
+    {
+      instNum ++;
+    }
+  return instNum;
+}
+
+List<Instruction> getInstruction(int roomNumber){
+  // Should add function in Valhalla to get the route/number of Instruction given the room number
+  int numberOfInstruction = 1;
+  List<Instruction> instructions = new List<Instruction>();
+  for(int i = 0; i < numberOfInstruction; i++)
+    {
+      //Another function to get the instruction one by one
+      final inst1 = Instruction(direction: Direction.FORWARD, distance: 2.0);
+      final inst2 = Instruction(direction: Direction.RIGHT, distance: 5.5);
+      final inst3 = Instruction(direction: Direction.LEFT, distance: 0.5);
+      final inst4 = Instruction(direction: Direction.FORWARD, distance: 5.0);
+      final inst5 = Instruction(direction: Direction.LEFT, distance: 20.0);
+      instructions.add(inst1);
+      instructions.add(inst2);
+      instructions.add(inst3);
+      instructions.add(inst4);
+      instructions.add(inst5);
+    }
+  return instructions;
+}
+
 class _NavigationScreenState extends State<NavigationScreen> {
-  final inst1 = Instruction(direction: Direction.FORWARD, distance: 2.0);
-  final inst2 = Instruction(direction: Direction.RIGHT, distance: 5.5);
-  final inst3 = Instruction(direction: Direction.LEFT, distance: 0.5);
-  final inst4 = Instruction(direction: Direction.FORWARD, distance: 5.0);
+  List<Instruction> instList = getInstruction(110);
+  int instNum = 0;
+  FlutterTts flutterTts = FlutterTts();
   @override
   Widget build(BuildContext context) {
     final room_model = Provider.of<RoomModel>(context);
+
     return Scaffold(
       appBar: AppBar(
         leading: IconButton(
@@ -50,21 +83,44 @@ class _NavigationScreenState extends State<NavigationScreen> {
           padding: EdgeInsets.fromLTRB(15.0, 20.0, 15.0, 0.0),
           children: <Widget>[
             CurrentInstruction(
-              instruction: inst1,
+              instruction: instList[0],
             ),
             InstructionTile(
-              instruction: inst2,
+              instruction: instList[1],
             ),
             InstructionTile(
-              instruction: inst3,
+              instruction: instList[2],
             ),
             InstructionTile(
-              instruction: inst4,
+              instruction: instList[3],
             ),
-            CardTile(titleText: "Room ${room_model.room.name} should be on your Right",)
+            InstructionTile(
+              instruction: instList[4],
+            ),
+            CardTile(titleText: "Room ${room_model.room.name} should be on your Right",),
+
+            new ListTile(
+              leading: new Icon(Icons.plus_one, color: Colors.black),
+              onTap: () => positionUpdate( context, room_model ),
+            )
           ],
         ),
       ),
     );
+  }
+
+  positionUpdate(BuildContext context, final room_model) async {
+    debugPrint( "On Press" );
+//    if( instNum < getUpdate(100, 20, 98, 23, instNum) )
+    {
+      instNum++;
+      debugPrint('$instNum');
+      flutterTts.speak( instList[instNum].text );
+    }
+    final result = await Navigator.push((context),
+      MaterialPageRoute(builder: (context) => build(context) ) , );
+    setState(() {
+
+    });
   }
 }
