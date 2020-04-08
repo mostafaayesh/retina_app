55b03cb9dc99de5eb00bede7a8b3b8c51c01b8dd
jzpnhlim <50381643+jzpnhlim@users.noreply.github.com>
2020-04-08 16:51:13 -0400

    get all access to nominatim output
    added seperate getGlobalCoordinates(NominatimOutput) function
    
    tested

--- test/nominatim_test.dart
+++ test/nominatim_test.dart
@@ -5,10 +5,11 @@ import 'package:retina/utils/nominatim.dart';
 
 void main() async {
   test('getGlobalCoordinates test', () async {
-    final testCoords = await getGlobalCoordinates(
+    final testn = await getNominatimOutput(
       "nominatim.openstreetmap.org",
       "Information Technology Building McMaster",
     );
+    GlobalCoordinates testCoords= getGlobalCoordinate(testn);
     var trueCoords = GlobalCoordinates(
       latitude: 43.258860150000004,
       longitude: -79.92078009725824,
